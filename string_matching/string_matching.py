85298f428376fe7eeeaa5c1be6ecafe079c1db8d
owis1998 <74670290+owis1998@users.noreply.github.com>
2021-01-18 14:23:14 +0200

    Update string_matching.py

--- string_matching/string_matching.py
+++ string_matching/string_matching.py
@@ -1,1 +1,10 @@
+def find(pattern, text):
+	for i in range(0, len(text) - len(pattern) + 1):
+		for j in range(0, len(pattern)):
+			if pattern[j] != text[i]:
+				break
+			i += 1
+		else:
+			return i - len(pattern)
 
+	return -1
